8af3cca941933860fcfe73f857ae1b88420a37dc
Hergusbergus <zane.ammerman@gmail.com>
2025-01-28 13:19:54 -0700

    Narrator Skeleton

---
+++ Assets/Scripts/TestAINarratorScripts/AINarratorTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class AINarratorTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() { print(Generate("The game just began. To the right of the player is a treasure chest, and to their left is the owner of the chest, whose guard is down...")); }
+
+    // Update is called once per frame
+    void Update() { }
+
+    string Generate(string prompt) { return "SAMPLE TEXT sample text"; }
+}
