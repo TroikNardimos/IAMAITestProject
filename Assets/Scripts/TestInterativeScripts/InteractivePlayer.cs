78b3aa8a9a9d30c320f6cba2908c0b4a8f7418c3
Troik-Nardimos <105459664+TroikNardimos@users.noreply.github.com>
2025-01-28 13:49:18 -0700

    Minor changes to function and aesthetic

--- Assets/Scripts/TestInterativeScripts/InteractivePlayer.cs
+++ Assets/Scripts/TestInterativeScripts/InteractivePlayer.cs
@@ -5,11 +5,6 @@ public class InteractivePlayer : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
     private Vector2 m_position;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/TestInterativeScripts/InteractivePopup.cs
+++ Assets/Scripts/TestInterativeScripts/InteractivePopup.cs
@@ -4,15 +4,4 @@ using UnityEngine;
 public class InteractivePopup : MonoBehaviour
 {
     public TMP_Text text;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
